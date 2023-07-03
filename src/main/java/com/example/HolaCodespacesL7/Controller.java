32ac1b1196e7b0a8b3c68778bc185bbf03f74ede
JulioRivero1996 <127893457+JulioRivero1996@users.noreply.github.com>
2023-07-03 13:39:48 +0000

    ec2

--- src/main/java/com/example/HolaCodespacesL7/Controller.java
+++ src/main/java/com/example/HolaCodespacesL7/Controller.java
@@ -3,6 +3,7 @@ package com.example.HolaCodespacesL7;
 import java.lang.String;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -12,6 +13,16 @@ public class Controller{
     @GetMapping(path="/")
     public String home (){
         return "PT72528736 - Julio Rivero";
+    }
+
+    @GetMapping(path="/idat/codigo")
+    public String codigo (){
+        return "PT72528736";
+    }
+
+    @GetMapping(path="/idat/nombre-completo")
+    public String nombrecompleto (){
+        return "Julio Aldair Rivero Crisostomo";
     }
 
     
--- src/main/java/com/example/HolaCodespacesL7/CursoRepository.java
+++ src/main/java/com/example/HolaCodespacesL7/CursoRepository.java
@@ -1,5 +1,7 @@
 package com.example.HolaCodespacesL7;
 
-public class CursoRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Integer> {
     
 }
